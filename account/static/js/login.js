7bb65e1aa2506567cf47034801f0a6fbfc4cf983
Anie22 <francisaniebiet678@gmail.com>
2024-08-25 09:45:04 +0100

    first commit

--- account/static/js/login.js
+++ account/static/js/login.js
@@ -50,7 +50,7 @@ form.addEventListener('submit', (e) => {
 
             if (data.message === 'Login successfully') {
                 // Redirect to the news validation form page if login was successful
-                window.location.href = '/detect-fake-news';
+                window.location.href = '/';
             }
         })
         .catch(error => console.error('Error:', error));
